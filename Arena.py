f534a209a66872d0134f6e2b7102cf2230a54269
Mario S. Mira <marioasmira@gmail.com>
2020-08-24 11:45:16 +0200

    Added all relevant functions

--- Arena.py
+++ Arena.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 import time
 import serial
 
@@ -27,9 +26,8 @@ class Arena:
         print(msg + "done");
 
 
-    def Init(self, dataPort)
+    def Init(self, dataPort):
         self.dataPort = dataPort;
-
         try:
             self.dataHandle = serial.Serial(self.dataPort, 115200, timeout=2)
             self.serialHandle = self.dataHandle
@@ -38,6 +36,48 @@ class Arena:
             print("Exiting program.")
             exit()
 
-t = Arena()
+    def SetBaseTemp(self, temp):
+        if temp < 5 or temp > 25:
+            raise Exception("Base temperature should be between 5 and 25 degrees.")
+        self.CheckSerialPort()
+        self.curTargetBaseTemp = temp
+        self.serialHandle.write(b"setcoppertemp=" + str(temp))
 
-t.Wait("Init... ", 5)
+    def CheckSerialPort(self):
+        if self.serialHandle == -1:
+            raise Exception("Serial port is not open. Call Arena.Init() first")
+
+    def Message(self, msg):
+        self.serialHandle.write(b"message=" + msg)
+        print("message="+msg)
+
+    def SetTileTemp(self, t1, t2, t3):
+        tempList = [t1, t2, t3]
+        if min(tempList) < 5 or max(tempList) > 60:
+            raise Exception("Tile temperature should be between 5 and 60 degrees")
+        self.CheckSerialPort()
+        self.serialHandle.write(b"settiletemp="+str(t1)+","+str(t2)+","+str(t3))
+
+    def Boost(self, b1, b2, b3):
+        boostList = [b1, b2, b3]
+        if sum(boostList) > 2:
+            raise Exception("Only two tiles can be boosted at the same time.")
+        self.CheckSerialPort()
+        self.serialHandle.write(b"boost="+str(b1)+","+str(b2)+","+str(b3))
+
+    def AreYouThere(self):
+        self.CheckSerialPort()
+        self.Message(self, "Yes I am")
+
+    def GetHandle(self):
+        return(self.serialHandle)
+
+    def Stop(self):
+        if self.serialHandle != -1:
+            self.serialHandle.close()
+        self.serialHandle = -1
+
+    def LED(self, l1, l2):
+        self.CheckSerialPort()
+        self.serialHandle.write(b"leds="+str(l1)+","+str(l2))
+
